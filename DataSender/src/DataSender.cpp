482b9ab4a98af07d0ccd26e6faff0e922cb42697
Vasile Sularea <sularea.vasile@gmail.com>
2024-07-22 20:33:56 +0200

    Adding some basic deep sleep for BT

--- DataSender/src/DataSender.cpp
+++ DataSender/src/DataSender.cpp
@@ -5,19 +5,27 @@
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 #define ADC_PIN 34
 
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  3        /* Time ESP32 will go to sleep (in seconds) */
+
 uint8_t adcValueBytes[4] = {0, 0, 0, 0};
 NimBLECharacteristic *pCharacteristic = nullptr;
-
+bool isNotified = false;
+uint16_t ogmillis = 0;
 
 class MyCallbacks: public NimBLECharacteristicCallbacks {
     void onRead(NimBLECharacteristic* pCharacteristic){
         Serial.print("Characteristic read by client: ");
         Serial.println(pCharacteristic->getUUID().toString().c_str());
+        isNotified = true;
+        Serial.println("Going to sleep now");
+        Serial.println("Time elapsed: ");
+        Serial.println(millis() - ogmillis);
     }
 };
 
-
 void setup() {
+    ogmillis = millis();
   Serial.begin(115200);
     NimBLEDevice::init("ESP32 Sensor");
     
@@ -40,23 +48,30 @@ void setup() {
 }
 
 void loop() {
-    long freeMem = ESP.getFreeHeap(); Serial.print("Remaining memory "); Serial.println(freeMem);
-    // Read ADC value
-    int adcValue = analogRead(ADC_PIN);
-    
-    // Convert integer to byte array
-    uint8_t adcValueBytes[4];
-    adcValueBytes[0] = (adcValue >> 24) & 0xFF;
-    adcValueBytes[1] = (adcValue >> 16) & 0xFF;
-    adcValueBytes[2] = (adcValue >> 8) & 0xFF;
-    adcValueBytes[3] = adcValue & 0xFF;
+    if( isNotified == false)
+    {
+        delay(300);
+        long freeMem = ESP.getFreeHeap(); Serial.print("Remaining memory "); Serial.println(freeMem);
+        // Read ADC value
+        int adcValue = analogRead(ADC_PIN);
+        
+        // Convert integer to byte array
+        uint8_t adcValueBytes[4];
+        adcValueBytes[0] = (adcValue >> 24) & 0xFF;
+        adcValueBytes[1] = (adcValue >> 16) & 0xFF;
+        adcValueBytes[2] = (adcValue >> 8) & 0xFF;
+        adcValueBytes[3] = adcValue & 0xFF;
 
-    // Update characteristic value
-    pCharacteristic->setValue(adcValueBytes, 4);
-    pCharacteristic->notify();
-    
-    Serial.print("ADC Value: ");
-    Serial.println(adcValue);
-
-    delay(1000);
+        // Update characteristic value
+        pCharacteristic->setValue(adcValueBytes, 4);
+        pCharacteristic->notify();
+        
+        Serial.print("ADC Value: ");
+        Serial.println(adcValue);
+    }
+    else
+    {   
+        esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+        esp_deep_sleep_start();
+    }
 }
